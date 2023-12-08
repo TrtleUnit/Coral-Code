0bc18ab6f648529fed7af4e91f6b84b05ad7bc7a
TrtleUnit <113350372+TrtleUnit@users.noreply.github.com>
2023-12-08 13:45:02 +0100

    CoralCode Bestand
    oopsie

---
+++ MauiApp CoralCode/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp_CoralCode;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MauiApp CoralCode/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiApp_CoralCode;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
